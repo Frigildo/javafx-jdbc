9c1691d55c4531f1fb497303c66dd30731115cb0
Frigildo Zunguze <zunguzefrigildo@gmail.com>
2025-04-28 14:45:12 +0200

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
 
 public class MainViewController implements Initializable{
 	
@@ -30,7 +39,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("MenuItemAbout");
+		loadView("/gui/About.fxml");
 	}
 
 	@Override
@@ -38,4 +47,20 @@ public class MainViewController implements Initializable{
 		
 	}
 
+	public synchronized void loadView(String pathView) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(pathView));
+			VBox newVbox = loader.load();
+			
+			Scene mainView = Main.getMainScene();
+			VBox mainVbox = ((VBox) ((ScrollPane) mainView.getRoot()).getContent());
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+		} catch (IOException e) {
+			Alerts.showAlerts(pathView, pathView, pathView, null);
+		}
+	}
 }
